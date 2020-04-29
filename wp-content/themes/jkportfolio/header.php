47d13e11002bf007ef8095f68c0044fb81254f61
Jaydon Kerr <62623891+Jaydon-Kerr@users.noreply.github.com>
2020-04-29 13:31:21 -0700

    Added more basic design
    Added font and image placeholder. Added Basic Colors. Started implementation of spacing and padding elements.

--- wp-content/themes/jkportfolio/header.php
+++ wp-content/themes/jkportfolio/header.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo('charset') ?>">
+	<head>
+		<meta charset="<?php bloginfo('charset') ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+		<script src="https://kit.fontawesome.com/3b8e917ff1.js" crossorigin="anonymous"></script>
 		<?php wp_head(); ?>
-</head>
+	</head>
 <body>
 	<div class="container">
 		<!-- header will float left -->
 		<header id="left-bar">
 			<nav>
 				<a href="#">
-					<h1 class="large-heading">Jaydon</h1>
-					<h1 class="large-heading">Kerr</h1>
+					<h1 class="large-heading highlight">Jaydon</h1>
+					<h1 class="large-heading highlight">Kerr</h1>
 				</a>
 				<ul>
 					<li><a href="#">Portfolio</a></li>
--- wp-content/themes/jkportfolio/index.php
+++ wp-content/themes/jkportfolio/index.php
@@ -4,26 +4,62 @@
 		<!-- right float content -->
 		<main id="right-bar">
 			<div class="content-header">
-				<h1>Hello, I'm Jaydon.</h1>
-				<h1>I'm a web designer and developer.</h1>
+				<h1>Hello, I'm <span class="highlight">Jaydon</span>.</h1>
+				<h1>I'm a <span class="highlight">web designer</span> and <span class="highlight">developer</span>.</h1>
 				<video width="1280" height="720" controls>
 					<source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
 					Your browser does not support video tags - sad day :(
 				</video>
 				<!-- downward arrow icon goes here -->
-				<p class="centered">See what I can do.</p>
+				<div class="centered">
+					<p>See what I can do.</p>
+					<i class="fas fa-long-arrow-alt-down fa-5x"></i>
+				</div>
+				
 
 				<div class="book-nav centered">
-					<h5><a href="#" class="book-nav__link">Websites</a></h5>
-					<h5><a href="#" class="book-nav__link">JavaScript/jQuery API's</a></h5>
-					<h5><a href="#" class="book-nav__link">Graphics</a></h5>
+					<ul class="book-nav__list">
+						<li><a href="#" class="book-nav__link">Websites</a></li>
+						<li><a href="#" class="book-nav__link">JavaScript/jQuery API's</a></li>
+						<li><a href="#" class="book-nav__link">Graphics</a></li>
+					</ul>
 				</div>
 			</div>
 			<div>
-				<div class="content-project">
+				<div class="content-project content-project--teal">
 					<h2>Websites</h2>
 
-					<?php
+					<?php 
+						
+						$websites = new WP_Query(array(
+							'posts_per_page' => -1,
+							'post_type' => 'website',
+							'orderby' => 'date',
+							'order' => 'DESC'
+							)
+						);
+
+						while ($websites->have_posts())
+						{
+
+							$websites->the_post();
+							// fetch the featured image
+							$featured_image = get_field('featured_image');
+							$featured_image_url = $featured_image['sizes']['large']; // (thumbnail, medium, large, full or custom size)
+							?> 
+							<div class="content-project__container content-project__container--teal">
+								<img class="content-project__image" src="<?php echo esc_url($featured_image_url); ?>">
+								<h5><?php the_title(); ?></h5> 
+							</div>
+							<?php
+						}
+
+						wp_reset_postdata();
+						
+					?>
+
+					<?php 
+						/*
 						$websites = new WP_Query(array(
 							'posts_per_page' => -1,
 							'post_type' => 'website',
@@ -71,6 +107,7 @@
 						}
 
 						wp_reset_postdata();
+						*/
 					?>
 
 				</div>
